5c0c833318448546b003306e6070d070ec612b60
Emerson Jesus <Emersoncruz73@hotmail.com>
2023-11-25 14:00:15 -0300

    percent button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const allClearButtons = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 const negativeButton = document.querySelector('[data-negative]');
+const percentageButton = document.querySelector('[data-percent]');
 
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
@@ -16,7 +17,7 @@ class Calculator {
     calculate() {
         let result;
 
-        const  _previousOperand = parseFloat(this.previousOperand);
+        const _previousOperand = parseFloat(this.previousOperand);
         const _currentOperand = parseFloat(this.currentOperand);
 
         if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
@@ -49,7 +50,7 @@ class Calculator {
     }
 
     chooseOperation(operation) {
-        if  (this.currentOperand === '') return;
+        if (this.currentOperand === '') return;
 
         if (this.previousOperand !== "") {
             this.calculate();
@@ -61,34 +62,71 @@ class Calculator {
         this.currentOperand = '';
     }
 
-    appendNumber(number)  {
+    appendNumber(number) {
         if (this.isResultDisplayed && this.operation === undefined) {
             this.clear();
             this.isResultDisplayed = false;
         }
 
-        if (this.currentOperand.includes(".")&& number === ".") return;
-        this.currentOperand  = `${this.currentOperand}${number.toString()}`;
+        if (this.currentOperand.includes(".") && number === ".") return;
+        this.currentOperand = `${this.currentOperand}${number.toString()}`;
     }
 
     clear() {
         this.currentOperand = "";
         this.previousOperand = "";
         this.operation = undefined;
+        this.isResultDisplayed = false;
     }
 
-    updateDisplay(){
-        this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation || ""}`;
-        this.currentOperandTextElement.innerText = this.currentOperand;
+    updateDisplay() {
+        this.previousOperandTextElement.textContent = `${this.previousOperand} ${this.operation || ""}`;
+        this.currentOperandTextElement.textContent = this.currentOperand;
     }
 
     toggleNegative() {
         this.currentOperand = (parseFloat(this.currentOperand) * -1).toString();
-    }    
+    }
+
+    calculatePercentage() {
+        const currentOperandValue = parseFloat(this.currentOperand);
+        const previousOperandValue = parseFloat(this.previousOperand);
+
+        if (!isNaN(currentOperandValue)) {
+            if (!isNaN(previousOperandValue) && this.operation) {
+                switch (this.operation) {
+                    case '+':
+                        this.currentOperand = (previousOperandValue + (previousOperandValue * currentOperandValue / 100)).toString();
+                        break;
+                    case '-':
+                        this.currentOperand = (previousOperandValue - (previousOperandValue * currentOperandValue / 100)).toString();
+                        break;
+                    case 'x':
+                        this.currentOperand = (previousOperandValue * (currentOperandValue / 100)).toString();
+                        break;
+                    case '÷':
+                        if (currentOperandValue === 0) {
+                            alert("Não é possível dividir por zero");
+                            this.clear();
+                            return;
+                        }
+                        this.currentOperand = (previousOperandValue / 100 * currentOperandValue).toString();
+                        break;
+                    default:
+                        return;
+                }
+                this.previousOperand = '';
+                this.operation = undefined;
+                this.isResultDisplayed = true;
+            } else {
+                this.currentOperand = (currentOperandValue / 100).toString();
+            }
+        }
+    }
 }
 
-const  calculator = new Calculator(
-    previousOperandTextElement, 
+const calculator = new Calculator(
+    previousOperandTextElement,
     currentOperandTextElement
 );
 
@@ -104,19 +142,24 @@ for (const operationButton of operationButtons) {
         calculator.chooseOperation(operationButton.innerText);
         calculator.updateDisplay();
     });
-} 
+}
 
 allClearButtons.addEventListener('click', () => {
     calculator.clear();
     calculator.updateDisplay();
 });
 
-equalsButton.addEventListener("click", ()=>  {
+equalsButton.addEventListener("click", () => {
     calculator.calculate();
     calculator.updateDisplay();
 });
 
 negativeButton.addEventListener('click', () => {
     calculator.toggleNegative();
+    calculator.updateDisplay();
+});
+
+percentageButton.addEventListener('click', () => {
+    calculator.calculatePercentage();
     calculator.updateDisplay();
 });
